cdbb2cdab31bee3e61b2cafef21f5f6bec5f6809
aquaform <aquaform@yandex.ru>
2017-03-29 17:08:45 +0500

    Сделал меню

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
 $(function() {
 
-	// Custom JS
+	$('#my-menu').mmenu({
+		extensions: [ 'widescreen','theme-black', 'effect-menu-slide', 'pagedime-black' ],
+		navbar: {
+			title: '<img src="img/logo-1.svg" alt="Салон красоты Смитлер">'
+		},
+		offCanvas: {
+			position: 'right'
+		}
+	});
+
+
+
+	var api = $('#my-menu').data('mmenu');
+	api.bind('opened', function() {
+		$('.hamburger').addClass('is-active');
+	}).bind('closed', function() {
+		$('.hamburger').removeClass('is-active');
+	});
 
 });
